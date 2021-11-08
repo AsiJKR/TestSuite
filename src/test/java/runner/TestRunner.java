eb5cbc2c599edbfb0f5a2305950b5bbb43c09237
asithawijekoon <asitha.wijekoon@eyewa.com>
2021-11-09 02:14:48 +0530

    Including + failure screenshots append for the report - examples are committed for reference - pico container removed

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -5,27 +5,22 @@ import io.cucumber.java.After;
 import io.cucumber.java.Scenario;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
-
-import java.io.File;
-import java.io.IOException;
 
 
 @CucumberOptions(
         features = {"src/test/resources/features/ATGHomePage.feature"},
-        glue = {"steps"},
+        glue = {"steps","runner"},
         plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:","summary"},
         publish = true
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
 
-    DriverHandler driverHandler = new DriverHandler();
+    public DriverHandler driverHandler = new DriverHandler();
 
     @Override
     @DataProvider(parallel = true)
@@ -38,20 +33,14 @@ public class TestRunner extends AbstractTestNGCucumberTests {
         driverHandler.initialize();
     }
 
-    @AfterSuite
-    public void sc(Scenario scenario) throws IOException {
-                if (scenario.isFailed()){
-        File screenshot = ((TakesScreenshot) driverHandler.getDriver()).getScreenshotAs(OutputType.FILE);
-        byte[] fileContent = FileUtils.readFileToByteArray(screenshot);
-        scenario.attach(fileContent, "image/png", "screenshot");
+    @After
+    public void cleanUp(Scenario scenario) {
+        if (scenario.isFailed()) {
+            // Take a screenshot
+            final byte[] screenshot = ((TakesScreenshot) driverHandler.getDriver()).getScreenshotAs(OutputType.BYTES);
+            // embed it in the report
+            scenario.attach(screenshot, "image/png", "failure screenshot");
         }
         driverHandler.tearDown();
     }
-
-//    @AfterMethod
-    public void tearDown()  {
-        driverHandler.tearDown();
-    }
-
-
 }
